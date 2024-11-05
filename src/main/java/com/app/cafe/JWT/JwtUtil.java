9d69526f4ee74744c1eee68958b8596004054ac9
Mitesh Jethva <miteshjethva28@gmail.com>
2024-11-05 22:43:27 +0530

    Project dependecy configured and JWT util added

---
+++ src/main/java/com/app/cafe/JWT/JwtUtil.java
@@ -0,0 +1,83 @@
+package com.app.cafe.JWT;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Function;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtUtil {
+
+	private String secret = "miteshjethva2811";
+	
+	private SecretKey getKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(secret);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	public String extractUsername(String token) {
+		return extractClaims(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration(String token) {
+		return extractClaims(token, Claims::getExpiration);
+	}
+	
+	public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	public Claims extractAllClaims(String token) {
+		return Jwts.parser().verifyWith(getKey()).build().parseSignedClaims(token).getPayload();
+	}
+	
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+	
+	public String generateToken(String username, String role) {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("role", role);
+		return createToken(claims, username);
+		
+	}
+	private String createToken(Map<String, Object> claims, String subject) {
+//		return Jwts.builder()
+//				.setClaims(claims)
+//				.setSubject(subject)
+//				.setIssuedAt(new Date(System.currentTimeMillis()))
+//				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+//				.signWith(SignatureAlgorithm.HS256, secret).compact();
+		
+		
+		return Jwts.builder()
+				.claims()
+				.add(claims)
+				.subject(subject)
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+				.and()
+				.signWith(getKey())
+				.compact();
+	}
+	
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	
+	 
+}
